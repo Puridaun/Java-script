431ab8557e9f058a6ca162655c3ed4e4b6ab5f8b
Hritcu Serafim <hritcuserafim01@gmail.com>
2025-03-26 21:08:03 +0200

    Code improvement

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,6 +1,6 @@
 
 
-// ------- Constants ---------
+// ------- Variables ---------
 
 const form = document.querySelector('.tip-calculator-form');
 const percentButton = document.querySelectorAll('.percent-button');
@@ -11,51 +11,31 @@ const billAmountErr = document.querySelector('.bill-amount-error');
 const numOfPeopleErr = document.querySelector('.num-of-people-error');
 const standardInputFirst = document.querySelector('.standard-input-first');
 const standardInputSecond = document.querySelector('.standard-input-second');
+const customInput = document.querySelector('.custom-button');
+
 
 let tip = 0;
 let percentValue = 0;
 
+let tipPerPerson = 0;
+let totalPerPerson = 0;
 
+// ---------------History variables -----------------
 
-// ------ Funtions ----------
+const noBillValue = document.querySelector('.no-bill-value');
+const historyTip = document.querySelector('.tip-value');
+const dateValue = document.querySelector('.date-value');
 
-
-const selectButton = (e) => {
-
-    // console.log('selectButton() accesat')
-    // console.log(e)
-
-    const data = new FormData(form)
-    const buttonPressed = e.submitter;
-
-
-
-    percentButton.forEach(button => button.classList.remove('pressed-button'));
-    buttonPressed.classList.add('pressed-button');
-
-    // console.log(typeof (data.get('custom-percent')))
-
-    if (data.get('custom-percent')) {
-
-        percentValue = Number(data.get('custom-percent'));
-        percentButton.forEach(button => button.classList.remove('pressed-button'));
-        console.log("percentValueCustom is " + percentValue);
-        document.querySelector('.custom-button').value = "";
-        resetBtn.disabled = false;
-        resetBtn.style.backgroundColor = 'var(--green-400)'
-        return;
-
-    };
-
-    percentValue = Number(buttonPressed.innerText.slice(0, -1))
-    resetBtn.disabled = false;
-    resetBtn.style.backgroundColor = 'var(--green-400)'
-
-}
+const today = new Date;
+const day = today.getDate();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
 
 
 
 
+
+// ------ Functions -------------------------------
 
 const validateInput = () => {
 
@@ -63,18 +43,21 @@ const validateInput = () => {
 
     if (data.get('amount') <= 0) {
 
+        percentButton.forEach(button => button.classList.remove('pressed-button'));
         billAmountErr.style.display = 'block';
         standardInputFirst.style.borderColor = '#E17052';
-
+        customInput.value = "";
         return false;
     }
 
     if (data.get('number-of-people') <= 0) {
 
+        percentButton.forEach(button => button.classList.remove('pressed-button'));
         billAmountErr.style.display = 'none';
         numOfPeopleErr.style.display = 'block';
         standardInputSecond.style.borderColor = '#E17052';
         standardInputFirst.style.borderColor = 'var(--grey-50)';
+        customInput.value = "";
         return false;
     }
 
@@ -82,37 +65,67 @@ const validateInput = () => {
     standardInputSecond.style.borderColor = 'var(--grey-50)';
     numOfPeopleErr.style.display = 'none';
     billAmountErr.style.display = 'none';
-
     return true;
 }
 
-const calculatorResults = (e) => {
+const selectButton = (e) => {
 
-    // console.log(e);
+    customInput.value = "";
+    customInput.readOnly = true;
+    const buttonPressed = e.submitter;
+
+    percentButton.forEach(button => button.classList.remove('pressed-button'));
+    buttonPressed.classList.add('pressed-button');
+
+
+    percentValue = Number(buttonPressed.innerText.slice(0, -1))
+    resetBtn.disabled = false;
+    resetBtn.classList.remove('reset-button-disabled');
+    resetBtn.classList.add('reset-button-enabled');
+}
+
+const handleCustomInput = () => {
+
+    resetBtn.classList.remove('reset-button-disabled');
+    resetBtn.classList.add('reset-button-enabled');
+    resetBtn.disabled = false;
+    percentButton.forEach(button => button.classList.remove('pressed-button'));
+    percentValue = customInput.value;
+    calculateTipAndTotal();
+    showResults(tipPerPerson, totalPerPerson);
+}
+
+const selectCustomInput = () => {
+
+    if (validateInput()) {
+
+        customInput.readOnly = false;
+        customInput.addEventListener('input', handleCustomInput);
+    }
+}
+
+const calculateTipAndTotal = () => {
+
+    const data = new FormData(form);
+    const billAmount = Number(data.get('amount'));
+    const numPeople = Number(data.get('number-of-people'));
+
+    tip = billAmount * percentValue / 100;
+    tipPerPerson = (tip / numPeople).toFixed(2);
+    totalPerPerson = ((billAmount + tip) / numPeople).toFixed(2);
+}
+
+
+const calculatorsResults = (e) => {
+
+    e.preventDefault();
 
     if (validateInput()) {
 
         selectButton(e);
-        const data = new FormData(form)
-
-        const billAmount = Number(data.get('amount'));
-        const numPeople = Number(data.get('number-of-people'));
-
-
-        tip = billAmount * percentValue / 100;
-
-        // console.log("calculate tip is " + tip)
-
-        const tipPerPerson = (tip / numPeople).toFixed(2);
-        const totalPerPerson = ((billAmount + tip) / numPeople).toFixed(2);
-
-        // console.log('Tip per person:', tipPerPerson);
-        // console.log('Total per person:', totalPerPerson);
-
+        calculateTipAndTotal();
         showResults(tipPerPerson, totalPerPerson);
     }
-
-
 }
 
 
@@ -122,38 +135,47 @@ const calculatorResults = (e) => {
 const showResults = (tipValue, totalValue) => {
     tipAmount.innerText = `$${tipValue}`;
     total.innerText = `$${totalValue}`;
+
+    historyTip.innerText = percentValue;
+
 }
 
-
-
 const reset = () => {
-
+    console.log('test');
     form.reset();
     tipAmount.innerText = "$0.00";
     total.innerText = "$0.00";
-
     tip = 0;
+    customInput.value = "";
     percentButton.forEach(button => button.classList.remove('pressed-button'));
 
     numOfPeopleErr.style.display = 'none';
     billAmountErr.style.display = 'none';
 
-    resetBtn.style.backgroundColor = ''
+
+    resetBtn.classList.remove('reset-button-enabled');
+    resetBtn.classList.add('reset-button-disabled');
+    resetBtn.style.backgroundColor = '';
+    standardInputFirst.style.borderColor = 'var(--grey-50)';
+    standardInputSecond.style.borderColor = 'var(--grey-50)';
     resetBtn.disabled = true;
 }
-
-
-
-
-
 
 // --- Events ---
 
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    calculatorResults(e);
+
+    calculatorsResults(e);
 
 })
 
+customInput.addEventListener('click', selectCustomInput)
+
 resetBtn.addEventListener('click', reset)
+
+
+// ---------------History script -----------------
+
+
+dateValue.innerText = day + "." + month + "." + year;
